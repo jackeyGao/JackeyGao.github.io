b836e99b160e8dd2d1104824146cab9d31874db7
jackeyGao <gaojunqi@outlook.com>
2018-01-02 11:29:19 +0800

    donation

--- scripts/render.py
+++ scripts/render.py
@@ -77,6 +77,9 @@ template = template_env.get_template('about.html')
 output = template.render()
 with open('about.html', 'w') as f: f.write(output)
 
+template = template_env.get_template('donation.html')
+output = template.render()
+with open('donation.html', 'w') as f: f.write(output)
 
 #imgObjs = []
 #for album in albums:
